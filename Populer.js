71608358b1d03edbd2cf9e542029dd2195c89ebb
hyein89 <bihontai@gmail.com>
2025-06-10 01:15:47 +0700

    Update Populer.js

--- Populer.js
+++ Populer.js
@@ -1,1 +1,1 @@
-document.write('<script type='text/javascript' src='//abnormalitylovingmammal.com/81/5f/e7/815fe7d86ca8bba2fec79049a08e223e.js'></script>');
+document.write('<script type="text/javascript" src="//abnormalitylovingmammal.com/81/5f/e7/815fe7d86ca8bba2fec79049a08e223e.js"><\/script>');
